50a64d762f3456ea0874090669967e3b6ad0af46
Samuel Chan Ho Hin <samuel1995714@gmail.com>
2023-03-07 01:38:03 +0800

    update

--- ormconfig.js
+++ ormconfig.js
@@ -3,12 +3,12 @@ const SnakeNamingStrategy =
 
 module.exports = {
   type: "postgres",
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  // url: process.env.DATABASE_URL,
+  // host: process.env.DB_HOST,
+  // port: parseInt(process.env.DB_PORT),
+  // username: process.env.DB_USERNAME,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_DATABASE,
+  url: process.env.DATABASE_URL,
   synchronize: false,
   entities: ["src/entity/**/*.ts"],
   migrations: ["src/migration/**/*.ts"],
